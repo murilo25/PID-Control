d1ccf43cc987a5b4c184cd8a5b4a168aead1e886
Murilo Pinheiro <muriloaugusto25@hotmail.com>
2020-08-11 19:40:10 -0500

    increase kd gain

--- pid_control/main.cpp
+++ pid_control/main.cpp
@@ -37,7 +37,7 @@ int main() {
     /**
      * TODO: Initialize the pid variable.
      */
-    pid.Init(3, 2, 1);
+    pid.Init(3, 2, 4);
 
     h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char* data, size_t length,
         uWS::OpCode opCode) {
