88984695a13d69be49b7c71ad705034c172a69a9
Murilo Pinheiro <muriloaugusto25@hotmail.com>
2020-08-11 19:44:09 -0500

    try new gains

--- pid_control/main.cpp
+++ pid_control/main.cpp
@@ -37,7 +37,7 @@ int main() {
     /**
      * TODO: Initialize the pid variable.
      */
-    pid.Init(3, 2, 4);
+    pid.Init(10, 5, 5);
 
     h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char* data, size_t length,
         uWS::OpCode opCode) {
