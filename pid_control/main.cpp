c75e667c33472b81b57dd82458c930dbe13a33a1
Murilo Pinheiro <muriloaugusto25@hotmail.com>
2020-08-12 20:10:41 -0500

    change gains

--- pid_control/main.cpp
+++ pid_control/main.cpp
@@ -39,8 +39,8 @@ int main() {
      */
     // Set PID gains
     double k_p = 0.1;
-    double k_i = 0.0001;// 0.0001;
-    double k_d = 1.0;
+    double k_i = 0.005;// 0.0001;
+    double k_d = 4.0;// 1.0;
     pid.Init(k_p, k_i, k_d);
 
     h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char* data, size_t length,
