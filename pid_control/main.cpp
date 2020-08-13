57a5d0b74603ff86d7db46511e3fe3f0c9488a22
Murilo Pinheiro <muriloaugusto25@hotmail.com>
2020-08-12 19:57:10 -0500

    change gains

--- pid_control/main.cpp
+++ pid_control/main.cpp
@@ -38,8 +38,8 @@ int main() {
      * TODO: Initialize the pid variable.
      */
     // Set PID gains
-    double k_p = 0.01;
-    double k_i = 0.0003;// 0.0001;
+    double k_p = 0.1;
+    double k_i = 0.0005;// 0.0001;
     double k_d = 1.0;
     pid.Init(k_p, k_i, k_d);
 
